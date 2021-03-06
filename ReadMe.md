f77ce5ffece9fc0180b9c4a254b53b000478742f
AbsoluteAbsolver <64785006+AbsoluteAbsolver@users.noreply.github.com>
2021-03-06 16:44:49 +0100

    setup begin readme file and python script

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+This repositiory is for demonstating ewald summation
---
+++ ewald.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Calculate the electromagnetic potential in a periodic box. Fisrt we will show 
+The limitation width the traditional approach and then we will implement a 
+ewals summation.
+Created on Sat Mar  6 16:35:36 2021
+
+@author: Wesly
+"""
+
+import matplotlib as plt
+import numpy as np
+
+print('Hello World')
